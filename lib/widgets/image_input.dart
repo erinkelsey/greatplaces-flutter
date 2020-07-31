fa3e28105cf769ae7598926313dbe9979601948b
erinkelsey <erinszilagyi@gmail.com>
2020-07-30 19:58:14 -0500

    Add ability to open camera on device, take picture, and save picture to permanent storage on device. Also, create a new place with the saved image.

---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,77 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspath;
+
+class ImageInput extends StatefulWidget {
+  final Function onSelectImage;
+
+  ImageInput(this.onSelectImage);
+
+  @override
+  _ImageInputState createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File _storedImage;
+
+  Future<void> _takePicture() async {
+    final _picker = ImagePicker();
+    PickedFile imageFile = await _picker.getImage(
+      source: ImageSource.camera,
+      maxWidth: 600, // lower resolution -> app doesn't need hi-res
+    );
+
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+
+    // copy file to local permanent storage
+    final appDir = await syspath.getApplicationDocumentsDirectory();
+    final fileName = path.basename(_storedImage.path);
+    final savedImage = await _storedImage.copy('${appDir.path}/$fileName');
+    widget.onSelectImage(savedImage);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: 150,
+          height: 150,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Colors.grey,
+            ),
+          ),
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                )
+              : Text(
+                  'No Image Taken',
+                  textAlign: TextAlign.center,
+                ),
+          alignment: Alignment.center,
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: FlatButton.icon(
+            onPressed: () => _takePicture(),
+            icon: Icon(Icons.camera),
+            label: Text('Take Picture'),
+            textColor: Theme.of(context).primaryColor,
+          ),
+        ),
+      ],
+    );
+  }
+}
